74d8fb8942f8d4ec67ed5d18711af40449d957a0
Avuxo <sathwik45@gmail.com>
2023-01-26 17:45:28 -0500

    First commit!

--- iOSPreworkSpring/ViewController.swift
+++ iOSPreworkSpring/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+       
+       numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelfDidTapped(_sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I am in my \(year!) year at \(schoolNameTextField.text!). I own \(numberOfPetsLabel.text!) pets and it's true that I want more pets!"
+        
+        // Alert for our intro message
+        let alertController = UIAlertController(title: "myIntroduction", message: introduction, preferredStyle: .alert)
+        
+        // A way to dismiss the alerts
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        // Add the action we created to alert controller
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    
+    
+    
 }
 
